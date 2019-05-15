c10c9a81f30dbbc43e7eee56258dab3282cb2029
Oliver Mayer <mayer@adbk-nuernberg.de>
2019-05-15 19:02:41 +0200

    Langton ants app added

---
+++ ants/ants.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+/*
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2019 Oliver Mayer, Akademie der Bildenden Kuenste Nuernberg. 
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+
+Implementation of Langton's ant
+https://en.wikipedia.org/wiki/Langton%27s_ant
+
+required modules:
+    pygame   https://pypi.org/project/pygame/
+    numpy    https://pypi.org/project/numpy/
+ 
+"""
+
+import sys
+import pygame as pg
+import numpy as np
+import random
+from time import sleep
+
+# Ant class 
+# Stores position and handles movement of the ant according to a set of rules
+class Ant:
+
+    # Constructor: Specify ant's starting position in the stage with xstart, ystart
+    #              and starting direction. 
+    #              Directions are 0=left, 1=up, 2=right, 3=down
+    def __init__(self, xstart, ystart, direction=3):
+        self.x = xstart
+        self.y = ystart
+        self.dir = direction
+
+    # function to update ant direction and position according on the stage
+    def update(self, stage):
+    
+        # update ant's direction and value of the stage under the ant
+        if stage[self.y][self.x] == 0:
+            self.dir = (self.dir+1) % 4
+            stage[self.y][self.x] = 1
+        else:
+            if self.dir == 0:
+                self.dir = 3
+            else:
+                self.dir = self.dir-1
+            stage[self.y][self.x] = 0
+
+        # update ant's position
+        if self.dir == 0:    # go to left
+            if self.x == 0:
+                self.x = stage.shape[1]-1
+            else:
+                self.x = self.x - 1
+
+        elif self.dir == 1:  # go up
+            if self.y == 0:
+                self.y = stage.shape[0]-1
+            else:
+                self.y = self.y - 1
+
+        elif self.dir == 2:  # go right
+            self.x = (self.x + 1) % stage.shape[1]
+
+        else:                # go down
+            self.y = (self.y + 1) % stage.shape[0]
+
+    # draw ant at it's position with colorA. call after drawing the stage
+    def draw(self):
+        pg.draw.rect(screen, colorA, (self.x*cellSize, self.y*cellSize, cellSize, cellSize))
+
+
+# draw the stage with color0 for stage value = 0 and color1 for stage value = 1
+def drawStage( stageArray ):
+    
+    screen.fill(color0)
+    
+    for j in range(0, stageArray.shape[0]):
+        for i in range(0, stageArray.shape[1]):
+            if stageArray[j][i] == 1:
+                pg.draw.rect(screen, color1, (i*cellSize, j*cellSize, cellSize, cellSize))
+    return   
+
+       
+
+numCellsX = 100
+numCellsY = 100
+cellSize  = 5
+color0    = (255, 255, 255)
+color1    = (0, 0, 0)
+colorA    = (255, 0, 0)
+
+winWidth  = numCellsX*cellSize
+winHeight = numCellsY*cellSize
+
+# init pygame
+pg.init()
+screen = pg.display.set_mode( (winWidth, winHeight) )
+pg.display.set_caption('Langton ant')
+pg.mouse.set_visible(True)
+
+theStage = np.zeros( (numCellsY, numCellsX), dtype=np.uint8 )
+
+# one ant
+# ants = [Ant(50, 50)]
+
+# two ants evolving backward after a time
+# ants = [Ant(50, 50), Ant(40,23), Ant(10, 43), Ant(60, 20), Ant(74, 83)] 
+
+
+ants = [Ant(50, 50), Ant(40,23), Ant(10, 43, 1), Ant(60, 20, 2), Ant(74, 83, 0)] 
+
+while 1:
+    for event in pg.event.get():
+        if event.type == pg.QUIT: 
+            sys.exit()
+        if event.type == pg.MOUSEBUTTONUP:   # add new ant on mouse click
+            pos = pg.mouse.get_pos()
+            ants.append(Ant((int)(pos[0]/cellSize), (int)(pos[1]/cellSize)))
+
+    drawStage(theStage)
+    
+    for a in ants:
+        a.update(theStage)
+        a.draw()
+    
+    pg.display.flip()
+
